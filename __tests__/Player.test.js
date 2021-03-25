0f2a80dfd4cf1b4d570810ccd096d4ff353980e3
Danny <dannyp425@gmail.com>
2021-03-24 19:29:41 -0600

    Player tests

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -4,6 +4,9 @@ jest.mock("../lib/Potion");
 console.log(new Potion());
 
 const Player = require("../lib/Player");
+const { test, expect } = require("@jest/globals");
+const { palegreen } = require("color-name");
+const { extractExpectedAssertionsErrors } = require("expect");
 
 test("creates a player onject", ()=> {
     const player = new Player("Dave");
@@ -16,4 +19,52 @@ test("creates a player onject", ()=> {
         expect.arrayContaining([expect.any(Object)])
     );
     
+});
+
+test("gets player's stats as an object", ()=> {
+    const player = new Player("Dave");
+
+    expect(player.getStats()).toHaveProperty("potions");
+    expect(player.getStats()).toHaveProperty("health");
+    expect(player.getStats()).toHaveProperty("strength");
+    expect(player.getStats()).toHaveProperty("agility");
+});
+
+test("gets inventory from player or returns false", ()=> {
+    const player = new Player("Dave");
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    player.inventory = [];
+
+    expect(player.getInventory()).toEqual(false);
+});
+
+test("gets players health value", ()=> {
+    const player = new Player("Dave");
+
+    expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+});
+
+test("checks if player is alive or not", ()=> {
+    const player = new Player("Dave");
+
+    expect(player.isAlive()).toBeTruthy();
+
+    player.health = 0;
+
+    expect(player.isAlive()).toBeFalsy();
+});
+
+test("subtracts from player's health", ()=> {
+    const player = new Player("Dave");
+    const oldHealth = player.health;
+
+player.reduceHealth(5);
+
+expect(player.health).toBe(oldHealth - 5);
+
+player.reduceHealth(9999);
+
+expect(player.health).toBe(0);
 });
